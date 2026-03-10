b41743832f06fac2252150f46055e1507eb3134e
Ahti <ahtisyri1@gmail.com>
2026-03-10 13:36:35 +0200

    Add pins.h

---
+++ src/pins.h
@@ -0,0 +1,10 @@
+/**
+ * @file tkjhat/pins.h
+ */
+
+/** @name Buttons / Switches
+ *  @{
+ */
+#define SW1_PIN               2   /**< SW1 button pin (GPIO 2) */
+#define SW2_PIN               22  /**< SW2 button pin (GPIO 22) */
+/** @} */
